3817c006db4edcea5b98bc292d3f3331812dee90
Juan Pablo Franco <jfranco@ooyala.com>
2016-09-01 15:55:26 -0700

    Disabling Hazmat logging by default

--- js/utils/InitModules/InitOOHazmat.js
+++ js/utils/InitModules/InitOOHazmat.js
@@ -1,8 +1,21 @@
 require("./InitOOUnderscore.js");
 
+var hazmatConfig = {};
+
+// 'debugHazmat' flag needs to be set before plugins are loaded. If we added
+// this flag to the OO namespace, it would be overriden during plugin initalization,
+// so we need to use a global var instead
+if (window && !window.debugHazmat) {
+  hazmatConfig = {
+    fail: function() { return; },
+    warn: function() { return; },
+    log: function() { return; }
+  };
+}
+
 if ((!OO.HM) && (typeof window === 'undefined' || typeof window._ === 'undefined'))
 {
-  OO.HM = require('hazmat').create();
+  OO.HM = require('hazmat').create(hazmatConfig);
 }
 else if (!window.Hazmat)
 {
@@ -11,5 +24,5 @@ else if (!window.Hazmat)
 
 if (!OO.HM)
 {
-  OO.HM = window.Hazmat.noConflict().create();
+  OO.HM = window.Hazmat.noConflict().create(hazmatConfig);
 }
