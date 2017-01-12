b31d2af6d30c019ffc76d76544ca2a37e72e4c65
Dimitry Nasledov <dimitry.nasledov@ooyala.com>
2017-01-11 16:31:33 -0800

    [PLAYER-65] Fixed typo in comment

--- js/utils/environment.js
+++ js/utils/environment.js
@@ -284,7 +284,7 @@
      * Determines if a single video element should be used.<br/>
      * <ul><li>Use single video element on iOS, all versions</li>
      *     <li>Use single video element on Android, all versions</li></ul>
-     * 01/11/17 Previous JDDoc for Android - to be removed once fix is confirmed and there is no regression:<br />
+     * 01/11/17 Previous JSDoc for Android - to be removed once fix is confirmed and there is no regression:<br />
      * <ul><li>Use single video element on Android < v4.0</li>
      *     <li>Use single video element on Android with Chrome < v40<br/>
      *       (note, it might work on earlier versions but don't know which ones! Does not work on v18)</li></ul>
